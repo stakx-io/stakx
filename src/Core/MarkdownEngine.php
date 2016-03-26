b721fece1167f44fbe30b094569d2151db39e111
allejo <allejo@me.com>
2016-03-25 19:56:01 -0700

    Fix code highlighting with no language or unknown language

--- src/Core/MarkdownEngine.php
+++ src/Core/MarkdownEngine.php
@@ -16,9 +16,17 @@ class MarkdownEngine extends \Parsedown
     public function blockFencedCodeComplete($block)
     {
         // The class has a `language-` prefix, remove this to get the language
-        $language = substr($block['element']['text']['attributes']['class'], 9);
-        $highlighted = $this->highlighter->highlight($language, $block['element']['text']['text']);
-        $block['element']['text']['text'] = $highlighted->value;
+        if (isset($block['element']['text']['attributes']))
+        {
+            $language = substr($block['element']['text']['attributes']['class'], 9);
+
+            try
+            {
+                $highlighted = $this->highlighter->highlight($language, $block['element']['text']['text']);
+                $block['element']['text']['text'] = $highlighted->value;
+            }
+            catch (\DomainException $exception) {}
+        }
 
         return $block;
     }
