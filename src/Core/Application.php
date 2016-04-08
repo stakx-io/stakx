a9a47cc6efba8dbc6969e475b369790a9e570b4c
allejo <vladimir.jimenez.102@my.csun.edu>
2016-04-07 21:11:42 -0700

    Output notice regarding xdebug being loaded

---
+++ src/Core/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace allejo\stakx\Core;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Application extends \Symfony\Component\Console\Application
+{
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        if (extension_loaded('xdebug') && !getenv('COMPOSER_DISABLE_XDEBUG_WARN'))
+        {
+            $output->writeln('<fg=black;bg=yellow>You are running Stakx with xdebug enabled. This has a major impact on runtime performance.</>');
+        }
+
+        return parent::doRun($input, $output);
+    }
+}
