98ed1f6d45937ec28ac7cf26bf9753b319e3a14d
allejo <allejo@me.com>
2017-01-06 03:28:58 -0800

    Twig's url() function now supports FM Objects
    Passing a PageView or ContentItem to the `url()` Twig function will automatically
    use the permalink. There's no need to explicitly call the `permalink` function

--- src/Twig/BaseUrlFunction.php
+++ src/Twig/BaseUrlFunction.php
@@ -2,6 +2,7 @@
 
 namespace allejo\stakx\Twig;
 
+use allejo\stakx\Object\FrontMatterObject;
 use Twig_Environment;
 
 class BaseUrlFunction
@@ -9,6 +10,11 @@ class BaseUrlFunction
     public function __invoke (Twig_Environment $env, $assetPath)
     {
         $globals = $env->getGlobals();
+
+        if ($assetPath instanceof FrontMatterObject)
+        {
+            $assetPath = $assetPath->getPermalink();
+        }
 
         // @TODO 1.0.0 Remove support for 'base' as it's been deprecated
         $base = (array_key_exists('base', $globals['site'])) ? $globals['site']['base'] : $globals['site']['baseurl'];
