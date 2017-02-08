a0a3a4e3e4e09b4fcc47424a945a9ee89310b7de
allejo <vladimir.jimenez.102@my.csun.edu>
2017-02-07 19:37:16 -0800

    Add some unit tests for front matter file format

---
+++ tests/FrontMatterObjectTests/FrontMatterFormatTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace FrontMatterObjectTests;
+
+use allejo\stakx\Exception\InvalidSyntaxException;
+use allejo\stakx\Object\ContentItem;
+
+class FrontMatterFormatTest extends \PHPUnit_Stakx_TestCase
+{
+    public function testBlankLineAfterFrontMatter ()
+    {
+        $expectedFormat = <<<LINE
+---
+foo: bar
+---
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testNoBlankLineAfterFrontMatter ()
+    {
+        $expectedFormat = <<<LINE
+---
+foo: bar
+---
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testEmptyFrontMatter ()
+    {
+        $expectedFormat = <<<LINE
+---
+---
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testMultipleBlankLinesAfterFrontMatter ()
+    {
+        $expectedFormat = <<<LINE
+---
+foo: bar
+---
+
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testNoNewLineAfterFrontMatterThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---
+foo: bar
+---Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testNoNewLineAtBeginningOfFrontMatterThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---foo: bar
+---
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testFrontMatterInOneLineThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---foo: bar---
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testNoNewLineBeforeFrontMatterClosesThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---
+foo: bar---
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testNoBodyThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---
+foo: bar---
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testWhiteSpaceBodyThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+---
+foo: bar---
+
+
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    public function testEmptyFrontMatterOneLineThrowsException ()
+    {
+        $this->setExpectedException(InvalidSyntaxException::class);
+        $expectedFormat = <<<LINE
+------
+
+Some content
+LINE;
+
+        $this->createFile(ContentItem::class, $expectedFormat);
+    }
+
+    private function createFile ($classType, $content)
+    {
+        $this->dummyFile
+            ->setContent($content)
+            ->at($this->rootDir);
+
+        return (new $classType($this->dummyFile->url()));
+    }
+}
