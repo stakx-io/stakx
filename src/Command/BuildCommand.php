f270f30adc51ae8eaf746a314988480b3dccf796
Scott Wichser <blast007@users.sourceforge.net>
2017-02-08 21:02:05 -0600

    Include the file path for invalid syntax exceptions.

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -4,6 +4,7 @@ namespace allejo\stakx\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use allejo\stakx\Exception\InvalidSyntaxException;
 
 class BuildCommand extends BuildableCommand
 {
@@ -32,6 +33,13 @@ class BuildCommand extends BuildableCommand
 
             $output->writeln(sprintf("Your site built successfully! It can be found at: %s",
                 $this->website->getConfiguration()->getTargetFolder() . DIRECTORY_SEPARATOR
+            ));
+        }
+        catch (InvalidSyntaxException $e)
+        {
+            $output->writeln(sprintf("Your website failed to build with the following error in file '%s': %s",
+                $e->getPath(),
+                $e->getMessage()
             ));
         }
         catch (\Exception $e)
