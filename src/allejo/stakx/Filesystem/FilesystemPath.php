ffcd56f828ef1558d0920e1b7b694c5716a14355
Vladimir Jimenez <allejo@me.com>
2017-10-05 21:34:40 -0700

    Rename FSPath to FilesystemPath; made it more robust

---
+++ src/allejo/stakx/Filesystem/FilesystemPath.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace allejo\stakx\Filesystem;
+
+use allejo\stakx\System\Filesystem;
+
+/**
+ * A cross-platform filesystem path wrapper.
+ *
+ * This class is a wrapper for handling file paths in stakx in a cross-platform way. Give it a Windows path and append
+ * a Unix style path and it'll just work.
+ */
+final class FilesystemPath
+{
+    /** @var Filesystem */
+    private $fs;
+    /** @var string */
+    private $absolutePath;
+    /** @var string */
+    private $originalPath;
+    /** @var bool */
+    private $isWindows;
+
+    /**
+     * @param string $filePath
+     * @param string $dirSep
+     */
+    public function __construct($filePath, $dirSep = DIRECTORY_SEPARATOR)
+    {
+        $this->fs = new Filesystem();
+        $this->originalPath = $filePath;
+        $this->isWindows = ($dirSep === '\\');
+
+        if ($this->isWindows)
+        {
+            $filePath = $this->unixifyPath($filePath);
+        }
+
+        $this->absolutePath = ($this->fs->isAbsolutePath($filePath)) ? $filePath : $this->fs->absolutePath($filePath);
+    }
+
+    public function __toString()
+    {
+        return $this->getAbsolutePath();
+    }
+
+    /**
+     * Append a path to a directory path.
+     *
+     * @param string $append The path to append
+     */
+    public function appendToPath($append)
+    {
+        if ($this->isFile(false))
+        {
+            throw new \InvalidArgumentException("Appending to a file's path is not possible");
+        }
+
+        $this->absolutePath = $this->buildPath($this->absolutePath, $this->unixifyPath($append), '/');
+    }
+
+    /**
+     * Get the absolute path of the file path.
+     *
+     * @return string
+     */
+    public function getAbsolutePath()
+    {
+        if ($this->isWindows)
+        {
+            return str_replace('/', '\\', $this->absolutePath);
+        }
+
+        return $this->absolutePath;
+    }
+
+    /**
+     * Check whether the given path is a directory.
+     *
+     * @param bool $checkExistence When set to true, it will check the filesystem for the existence of the directory.
+     *                             When set to false, this function will guess based on the path ending in a directory
+     *                             separator.
+     *
+     * @return bool
+     */
+    public function isDir($checkExistence = true)
+    {
+        $absPath = $this->absolutePath;
+
+        if ($checkExistence)
+        {
+            return file_exists($absPath) && is_dir($absPath);
+        }
+
+        return (substr($absPath, -1, 1) == '/');
+    }
+
+    /**
+     * Check whether the given path is a file.
+     *
+     * @param bool $checkExistence When set to true, it will check the filesystem for the existence of the file. When
+     *                             set to false, this function will guess based on the path ending in a directory
+     *                             separator.
+     *
+     * @return bool
+     */
+    public function isFile($checkExistence = true)
+    {
+        $absPath = $this->absolutePath;
+
+        if ($checkExistence)
+        {
+            return file_exists($absPath) && is_file($absPath);
+        }
+
+        return (!$this->isDir($checkExistence));
+    }
+
+    /**
+     * Build a path from multiple strings.
+     *
+     * This function will _always_ use the '/' as the directory separator, because internal that's all stakx will use.
+     * The FilesystemPath::getAbsolutePath() function will worry about Windows paths when necessary.
+     *
+     * @return string
+     */
+    private function buildPath()
+    {
+        $paths = [];
+
+        foreach (func_get_args() as $arg)
+        {
+            if ($arg !== '')
+            {
+                $paths[] = $arg;
+            }
+        }
+
+        return preg_replace('#(?<!:)/+#', '/', join('/', $paths));
+    }
+
+    /**
+     * Convert a Windows path into a blasphemous Unix path.
+     *
+     * @param string $filePath
+     *
+     * @return string
+     */
+    private function unixifyPath($filePath)
+    {
+        return str_replace('\\', '/', $filePath);
+    }
+}
---
+++ tests/allejo/stakx/Test/Filesystem/FilesystemPathTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace allejo\stakx\Test\Filesystem;
+
+use allejo\stakx\Filesystem\FilesystemPath;
+use allejo\stakx\Test\PHPUnit_Stakx_TestCase;
+use org\bovigo\vfs\vfsStream;
+
+class FilesystemPathTest extends PHPUnit_Stakx_TestCase
+{
+    public function testCannotAppendToFilePath()
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        $path = new FilesystemPath('/root/bacon');
+        $path->appendToPath('vegan');
+    }
+
+    public function testFilePathIsDirectory()
+    {
+        $path = new FilesystemPath('/root/hello/');
+
+        $this->assertTrue($path->isDir(false));
+    }
+
+    public function testFilePathAppendDirectory()
+    {
+        $path = new FilesystemPath('/root/toast/', '/');
+        $path->appendToPath('/cheese/');
+
+        $this->assertEquals((string)$path, '/root/toast/cheese/');
+    }
+
+    public function testFilePathAppendWindowsDirectory()
+    {
+        $path = new FilesystemPath('/root/toast/', '/');
+        $path->appendToPath('\cheese\\');
+
+        $this->assertEquals((string)$path, '/root/toast/cheese/');
+    }
+
+    public function testWindowsFilePathAppendUnix()
+    {
+        $path = new FilesystemPath('C:\Users\stakx\\', '\\');
+        $path->appendToPath('/_site/');
+
+        $this->assertEquals('C:\Users\stakx\_site\\', (string)$path);
+    }
+
+    public function testWindowsFilePathAppendWindows()
+    {
+        $path = new FilesystemPath('C:\Users\stakx\\', '\\');
+        $path->appendToPath('\_site\\');
+
+        $this->assertEquals('C:\Users\stakx\_site\\', (string)$path);
+    }
+}
