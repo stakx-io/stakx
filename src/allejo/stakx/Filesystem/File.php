bf50f97ca3e715af3bf6aa553bbe2292cd78f9f5
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2017-10-17 23:39:21 -0700

    Update File docs and incorrect return values

--- src/allejo/stakx/Filesystem/File.php
+++ src/allejo/stakx/Filesystem/File.php
@@ -12,6 +12,8 @@ namespace allejo\stakx\Filesystem;
  *
  * This class extends \SplFileInfo and adds new methods along with overriding some methods solely because I feel that
  * some of the naming can be misleading.
+ *
+ * @since 0.2.0
  */
 final class File extends \SplFileInfo
 {
@@ -19,51 +21,110 @@ final class File extends \SplFileInfo
     private $relativeFilePath;
 
     /**
-     * @param string $file_name            The file name
+     * Constructor.
+     *
+     * @param string $filePath             The file name or absolute file path. If just a file name is given, then it
+     *                                     will look for the file in the current working directory.
      * @param string $relativeParentFolder The relative path
      * @param string $relativeFilePath     The relative path name
+     *
+     * @since 0.2.0
      */
-    public function __construct($file_name, $relativeParentFolder, $relativeFilePath)
+    public function __construct($filePath, $relativeParentFolder, $relativeFilePath)
     {
-        parent::__construct($file_name);
+        parent::__construct($filePath);
 
         $this->relativeParentFolder = $relativeParentFolder;
         $this->relativeFilePath = $relativeFilePath;
     }
 
+    /**
+     * Get the name of the file without an extension.
+     *
+     * @param  null $suffix This value will be discarded and is only needed to be able to override the \SplFileInfo
+     *                      definition.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getBasename($suffix = null)
     {
-        return parent::getBasename($this->getExtension());
+        return parent::getBasename('.' . $this->getExtension());
     }
 
+    /**
+     * Get the name of the with the extension.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getFilename()
     {
-        return $this->getBasename();
+        return parent::getBasename();
     }
 
+    /**
+     * Get the absolute path to this file.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getFilePath()
     {
         return $this->getPathname();
     }
 
+    /**
+     * Get the path to the parent folder of this file.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getParentFolder()
     {
         return $this->getPath();
     }
 
+    /**
+     * Get the file path to this file, relative to where it was created; likely the current working directory.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getRelativeFilePath()
     {
         return $this->relativeFilePath;
     }
 
+    /**
+     * Get the path to the parent folder this file, relative to where it was created; likely the current working directory.
+     *
+     * @since 0.2.0
+     *
+     * @return string
+     */
     public function getRelativeParentFolder()
     {
         return $this->relativeParentFolder;
     }
 
+    /**
+     * Get the contents of this file.
+     *
+     * @since 0.2.0
+     *
+     * @throws \RuntimeException When the file could not be read.
+     *
+     * @return string
+     */
     public function getContents()
     {
-        $content = file_get_contents($this->getPathname());
+        $content = file_get_contents($this->getFilePath());
 
         if ($content === false)
         {
