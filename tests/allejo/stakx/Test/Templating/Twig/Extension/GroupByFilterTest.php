9e858f70190e4e4349bbef3eab45b6f6d026f611
Vladimir Jimenez <allejo@me.com>
2018-05-22 22:20:38 -0700

    Add dot notation support to 'group' filter

--- tests/allejo/stakx/Test/Templating/Twig/Extension/GroupByFilterTest.php
+++ tests/allejo/stakx/Test/Templating/Twig/Extension/GroupByFilterTest.php
@@ -47,6 +47,41 @@ class GroupByFilterTests extends PHPUnit_Stakx_TestCase
 
         $this->assertEquals($expected, $grouped);
     }
+    public function testGroupByFilterDotNotation()
+    {
+        $original = [
+            'a' => ['metadata' => ['id' => 20], 'name' => 'chimpanzee'],
+            'b' => ['metadata' => ['id' => 40], 'name' => 'meeting'],
+            'c' => ['metadata' => ['id' => 20], 'name' => 'dynasty'],
+            'd' => ['metadata' => ['id' => 50], 'name' => 'chocolate'],
+            'e' => ['metadata' => ['id' => 10], 'name' => 'bananas'],
+            'f' => ['metadata' => ['id' => 50], 'name' => 'fantasy'],
+            'g' => ['metadata' => ['id' => 50], 'name' => 'football'],
+        ];
+
+        $expected = [
+            '10' => [
+                'e' => ['metadata' => ['id' => 10], 'name' => 'bananas'],
+            ],
+            '20' => [
+                'a' => ['metadata' => ['id' => 20], 'name' => 'chimpanzee'],
+                'c' => ['metadata' => ['id' => 20], 'name' => 'dynasty'],
+            ],
+            '40' => [
+                'b' => ['metadata' => ['id' => 40], 'name' => 'meeting'],
+            ],
+            '50' => [
+                'd' => ['metadata' => ['id' => 50], 'name' => 'chocolate'],
+                'f' => ['metadata' => ['id' => 50], 'name' => 'fantasy'],
+                'g' => ['metadata' => ['id' => 50], 'name' => 'football'],
+            ],
+        ];
+
+        $gbFilter = new GroupByFilter();
+        $grouped = $gbFilter($original, 'metadata.id');
+
+        $this->assertEquals($expected, $grouped);
+    }
 
     public function testGroupByFilterContentItems()
     {
