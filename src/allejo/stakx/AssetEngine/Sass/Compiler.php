eea5f92b83fa5a42c5600e66d17219b8e98983b2
Vladimir Jimenez <allejo@me.com>
2018-11-10 00:08:04 -0800

    Implement reusable Sass compiler for dev server
    Extend the original Sass compiler with a custom version that supports
    being reused so the dev server can serve updated Sass on reload without
    having to reparse all of the Sass and take advantage of the existing
    internal cache.

---
+++ src/allejo/stakx/AssetEngine/Sass/Compiler.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace allejo\stakx\AssetEngine\Sass;
+
+use Leafo\ScssPhp\Compiler as BaseCompiler;
+
+/**
+ * A modified version of our Sass compiler.
+ */
+class Compiler extends BaseCompiler
+{
+    /**
+     * @var int
+     */
+    protected $lastTouched;
+
+    /**
+     * Overridden method for handling Sass import statements.
+     *
+     * Our compiler will need to be able to refresh the Sass output if an import
+     * has changed since it was cached. This is used solely for the purpose of
+     * supporting our built-in dev server so that it can recompile Sass with the
+     * latest changes made in imports.
+     *
+     * @param string $path
+     * @param array  $out
+     */
+    protected function importFile($path, $out)
+    {
+        $realPath = realpath($path);
+
+        // If we've compiled our Sass once already, then the last modified time
+        // will exist in our listing. At this point, we already have Sass trees
+        // cached, so we should only clear the outdated ones.
+        if (isset($this->lastTouched[$realPath]))
+        {
+            // If the imported file has been modified since our last
+            // compilation, then we will need to clear the cached tree to allow
+            // our engine to build a new tree.
+            if (($t = filemtime($realPath)) > $this->lastTouched[$realPath])
+            {
+                $this->clearImportCache($realPath);
+                $this->lastTouched[$realPath] = $t;
+            }
+        }
+        else
+        {
+            $this->lastTouched[$realPath] = filemtime($realPath);
+        }
+
+        parent::importFile($path, $out);
+    }
+
+    /**
+     * Remove an import from the Sass cache.
+     *
+     * @param string|null $file The absolute path to an imported file that will
+     *                          be cleared from the cache. When given null, the
+     *                          entire cache will be cleared.
+     */
+    public function clearImportCache($file = null)
+    {
+        if ($file === null) {
+            $this->importCache = [];
+            return;
+        }
+
+        unset($this->importCache[$file]);
+    }
+}
