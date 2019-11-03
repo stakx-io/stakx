c636ba76f77e101da0020aa4d2ad83ea5ee1baec
Vladimir Jimenez <allejo@me.com>
2019-11-02 19:52:07 -0800

    Fix AppVeyor's broken chocolatey version
    https://github.com/chocolatey/choco/issues/1843

--- appveyor.bat
+++
@@ -1,7 +0,0 @@
-cd c:\tools\php*
-copy php.ini-production php.ini /Y
-echo date.timezone="UTC" >> php.ini
-echo extension_dir=ext >> php.ini
-echo extension=php_openssl.dll >> php.ini
-echo extension=php_mbstring.dll >> php.ini
-echo extension=php_fileinfo.dll >> php.ini
